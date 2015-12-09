94c77897b56d44086afc93f5526ab8dcf2ac0aad
kevin musgrove <kevin.musgrove@yahoo.com>
2015-12-09 10:51:11 -0600

    CH11Exceptions

---
+++ CH11Exceptions/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Mortgage
+    {
+        //properties
+        public string Name { get; set; }
+        public double CreditScore { get; set; }
+        public Mortgage(string name)
+        {
+            Name = name;
+
+
+        }
+        public bool EnterCreditScore(double credit)
+        {
+            bool resultRange = true;
+            if (credit >= 300 && credit <= 850)
+            {
+                return resultRange = true;
+            }
+            else if (credit >= 300 || credit <= 850)
+            {
+                resultRange = false;
+                throw new ArgumentException();
+            }
+            return resultRange;
+
+
+
+
+        }
+    }
+}
+
---
+++ CH11Exceptions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class MortgageClient
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            double credit = 0;
+            Console.WriteLine("enter a name:");
+            name = Console.ReadLine();
+            Mortgage test = new Mortgage(name);
+
+
+
+            do
+            {
+                try
+                {
+
+                    Console.WriteLine("Enter a credit score:");
+                    credit = double.Parse(Console.ReadLine());
+
+
+
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("not approved");
+
+                }
+            }
+            while (test.EnterCreditScore(credit));
+
+        }
+    }
+}
+}
