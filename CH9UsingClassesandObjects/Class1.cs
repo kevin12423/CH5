c1ccf0c65bb74c0967de2aad64514a293bf466d9
kevin musgrove <kevin.musgrove@yahoo.com>
2015-12-09 10:44:29 -0600

    CH9Classes

---
+++ CH9UsingClassesandObjects/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    class Tax
+    {
+        //properties
+        public string Ssn
+        {
+            get;
+            set;
+        }
+        public double GrossIncome
+        {
+            get;
+            set;
+        }
+        public readonly double TaxOwed;
+        //constructors
+        public Tax(string ssn, double grossIncome)
+        {
+            double taxRate;
+            Ssn = ssn;
+            GrossIncome = grossIncome;
+            if (grossIncome < 30000)
+            {
+                taxRate = .15;
+            }
+            else
+            {
+                taxRate = .28;
+            }
+            TaxOwed = grossIncome * taxRate;
+
+
+
+
+        }
+
+    }
+}
---
+++ CH9UsingClassesandObjects/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    class TaxClient
+    {
+        static void Main(string[] args)
+        {
+            Tax[] workers = new Tax[3];
+            string ssn = "";
+            double income;
+
+            for (int i = 0; i < workers.Length; i++)
+            {
+                Console.WriteLine("Enter a SSN");
+                ssn = Console.ReadLine();
+                Console.WriteLine("Enter Income");
+                income = Convert.ToDouble(Console.ReadLine());
+                workers[i] = new Tax(ssn, income);
+                Console.WriteLine("you need to pay:");
+                Console.WriteLine(workers[i].TaxOwed.ToString("C"));
+
+            }
+        }
+    }
+}
