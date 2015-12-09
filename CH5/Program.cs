04a5ace9e26bf09d1f7ff9ec3b294ee8498c1a8a
kevin musgrove <kevin.musgrove@yahoo.com>
2015-12-09 10:29:05 -0600

    Ch5Commit

---
+++ CH5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab12
+{
+    class Program
+    {
+        static void Main()
+        {
+            int total = 0;
+            var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+            Console.WriteLine("Enter a Sentence");
+            string sentence = Console.ReadLine().ToLower();
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (vowels.Contains(sentence[i]))
+                {
+                    total++;
+                }
+            }
+            Console.WriteLine("Your total number of vowels is: {0}", total);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CH5/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CH5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CH5")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fb5f6cd2-1659-48d6-9e70-9c03bd9daa09")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
