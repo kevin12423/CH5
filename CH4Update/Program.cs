c13590409a60d6917c3701b60d0a18a083a0d633
kevin musgrove <kevin.musgrove@yahoo.com>
2015-12-09 09:33:57 -0600

    firstOne

---
+++ CH4Update/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter r, p, s");
+            string play = Console.ReadLine();
+            Random ran = new Random();
+            int computer = ran.Next(1, 4);
+            switch (play)
+            {
+                case "r":
+                    if (computer == 1)
+                        Console.WriteLine("you tied they picked rock");
+                    if (computer == 2)
+                        Console.WriteLine("you lose they picked paper");
+                    if (computer == 3)
+                        Console.WriteLine("you win they picked scissors");
+                    break;
+                case "p":
+                    if (computer == 1)
+                        Console.WriteLine("you win they picked rock");
+                    if (computer == 2)
+                        Console.WriteLine("you tie they picked paper");
+                    if (computer == 3)
+                        Console.WriteLine("you lose they picked scissors");
+                    break;
+                case "s":
+                    if (computer == 1)
+                        Console.WriteLine("you lose they picked rock");
+                    if (computer == 2)
+                        Console.WriteLine("you win they picked paper");
+                    if (computer == 3)
+                        Console.WriteLine("you tie they picked scissors");
+                    break;
+            }
+
+
+        }
+    }
+}
